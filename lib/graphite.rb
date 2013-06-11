835e361ee9e22ee83430f3f76cdf1b41a81c549e
Matt Chadburn <matt.chadburn@guardian.co.uk>
2013-06-11 19:21:29 +0100

    Refactor graphite tool to accept arguments

--- lib/graphite.rb
+++ lib/graphite.rb
@@ -1,23 +1,22 @@
 require 'socket'
 
-
 class Graphite
     
     attr_accessor :key
 
     def initialize(opts)
-        @api_key = opts[:api_key] 
         @host = opts[:host]
         @port = opts[:port] || 2003
+        @verbose = opts[:verbose] || false
     end
         
     def log(opts)
-        time = Time.now.to_i
+        
         conn = TCPSocket.new @host, @port
         conn.puts "#{@api_key}.#{opts[:path]} #{opts[:value]}\n"
         conn.close
 
-        puts "#{@host} #{@port} #{@api_key}.#{opts[:path]} #{opts[:value]}\n"
+        puts "#{@host} #{@port} - #{opts[:path]} #{opts[:value]}\n"
 
     end
 
